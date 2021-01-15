ac21659e0cae1e96016bd063192df9a7d134d8b2
Sumanth_Varma <sumanthvarma.g07@gmail.com>
2021-01-15 11:45:29 +0530

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -25,8 +25,7 @@ export class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        console.log("current state is: " + JSON.stringify(values));
-        alert("current state is: " + JSON.stringify(values));
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render() {
@@ -94,7 +93,7 @@ function RenderDish({dish}) {
     );
 }
 
-function RenderComments({comments, postComment, dishId}) {
+function RenderComments({comments, addComment, dishId}) {
     if (comments != null) {
         return (
             <div className="col-12 col-md-5 m-1">
@@ -108,7 +107,7 @@ function RenderComments({comments, postComment, dishId}) {
                             </li>
                         );
                     })}
-                    <CommentForm />
+                    <CommentForm dishId={dishId} addComment={addComment} />
                 </ul>
             </div>
         );
@@ -136,7 +135,7 @@ const DishDetail = (props) => {
             </div>
             <div className="row">
                 <RenderDish dish={props.dish} />
-                <RenderComments comments={props.comments} />
+                <RenderComments comments={props.comments} addComment={props.addComment} dishId={props.dish.id} />
             </div>
             </div>
         )
